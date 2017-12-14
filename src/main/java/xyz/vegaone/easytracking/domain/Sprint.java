a70bde1e254b5245dacdbcf502aea4368efc633e
Rus David <davidrus.art@gmail.com>
2017-12-14 09:10:10 +0200

    Created a mock Sprint class for JUnit testing

--- src/main/java/xyz/vegaone/easytracking/domain/Sprint.java
+++ src/main/java/xyz/vegaone/easytracking/domain/Sprint.java
@@ -1,4 +1,23 @@
 package xyz.vegaone.easytracking.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "sprint")
 public class Sprint {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Override
+    public String toString() {
+        return "Sprint{" + "id=" + id + '}';
+    }
 }
